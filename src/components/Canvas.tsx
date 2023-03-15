0550a64439f6c79d48007a45acc29d59ba028f03
Egor Danilov <egordanilov970@gmail.com>
2023-03-14 21:48:48 -0600

    Canvas width now set to 100vw, when Sidebar is closed

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,15 +1,18 @@
 import { ReactElement } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 import styled from "styled-components";
 
-const CanvasWrapper = styled.div`
-    width: calc(100vw - 320px);
+const CanvasWrapper = styled.div<{sidebarIsOpen: boolean, nightModeIsOn: boolean}>`
+    width: ${props => props.sidebarIsOpen ? 'calc(100vw - 320px)' : '100vw'};
     background: #21212c;
     height: 100vh;
 `;
 
 function Canvas():ReactElement {
+    const uiState = useSelector((state: RootState) => state.ui);
     return(
-        <CanvasWrapper>
+        <CanvasWrapper sidebarIsOpen={uiState.sidebarIsOpen} nightModeIsOn={uiState.nightModeIsOn}>
             Canvas
         </CanvasWrapper>
     )
