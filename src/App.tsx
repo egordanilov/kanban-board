fe8e5288aa50986aca9df67d8599fd79c52da789
Egor Danilov <egordanilov970@gmail.com>
2023-03-14 21:20:49 -0600

    Redux Toolkit added

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,27 @@ import { ReactElement } from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
+import { store } from './store/store';
 import { Outlet, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import AddTask from './pages/AddTask';
 import Board from './pages/Board';
 import LandingPage from './pages/LandingPage';
 
 function App():ReactElement {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/board" element={<Board />} />
-        <Route path="/board/add-task" element={<AddTask />} />
-      </Routes>
-      <Outlet />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/board/add-task" element={<AddTask />} />
+        </Routes>
+        <Outlet />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
---
+++ src/store/uiSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+export interface UIState {
+    nightModeIsOn: boolean;
+    sidebarIsOpen: boolean;
+}
+
+const initialState: UIState = {
+    nightModeIsOn: true,
+    sidebarIsOpen: true,
+}
+
+export const uiSlice = createSlice({
+    name: 'ui',
+    initialState,
+    reducers: {
+        toggleNightMode: (state) => {
+            state.nightModeIsOn = !state.nightModeIsOn;
+        },
+        closeSidebar: (state) => {
+            state.sidebarIsOpen = false;
+        },
+        openSidebar: (state) => {
+            state.sidebarIsOpen = true;
+        }
+    },
+})
+
+export const { toggleNightMode, closeSidebar, openSidebar } = uiSlice.actions;
+
+export default uiSlice.reducer;
